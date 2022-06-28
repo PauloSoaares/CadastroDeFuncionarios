1c4c8af973b0bf524a2d9a645922009a88a4640a
Paulo Soares <paulohfrancelino1@hotmail.com>
2022-06-28 20:31:07 -0300

    tabela clientes e exibição da tabela  no menu

---
+++ js/scriptcli.js
@@ -0,0 +1,94 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-nome')
+const sServico = document.querySelector('#m-servico')
+const sPagar = document.querySelector('#m-pagar')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
+
+function openModal(edit = false, index = 0) {
+    modal.classList.add('active') // modal visivel
+
+    modal.onclick = e => {
+        if (e.target.className.indexOf('modal-container') !== -1) {
+            modal.classList.remove('active')
+        } // display none
+    }
+
+    if (edit) {
+        sNome.value = itens[index].nome
+        sServico.value = itens[index].servico
+        sPagar.value = itens[index].pagar
+        id = index
+    }   else {
+        sNome.value = ''
+        sServico.value = ''
+        sPagar. value = ''
+    }
+}
+
+function editItem(index) {
+
+    openModal(true, index)
+} // chamando a função "openModal"
+
+function deleteItem(index) {
+    itens.splice(index, 1)
+    setItensBD()
+    loadItens()
+}
+
+function insertItem(item, index) {
+    let tr = document.createElement('tr')
+  
+    tr.innerHTML = `
+      <td>${item.nome}</td>
+      <td>${item.servico}</td>
+      <td>R$ ${item.pagar}</td>
+      <td class="acao">
+        <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+      </td>
+      <td class="acao">
+        <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+      </td>
+    `
+    tbody.appendChild(tr)
+  }
+
+btnSalvar.onclick = e => {
+
+    if (sNome.value == '' || sServico.value == '' || sPagar.value == '') {
+        return
+    }
+
+    e.preventDefault();
+
+    if (id !== undefined) {
+        itens[id].nome = sNome.value
+        itens[id].funcao = sServico.value
+        itens[id].salario = sPagar.value
+    }   else {
+        itens.push({'nome': sNome.value, 'sevico': sServico.value, 'pagar': sPagar.value})
+    }
+
+    setItensBD()
+
+    modal.classList.remove('active')
+    loadItens()
+    id = undefined
+}
+
+function loadItens() {
+    itens = getItensBD()
+    tbody.innerHTML = ''
+    itens.forEach((item, index) => {
+        insertItem(item, index)
+    }) // carregando os dados do BD quando a pagina for carregada 
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfuncli')) ?? [] // pegando os itens do banco 
+const setItensBD = () => localStorage.setItem('dbfuncli', JSON.stringify(itens)) // setando os itens no BD
+
+loadItens()
